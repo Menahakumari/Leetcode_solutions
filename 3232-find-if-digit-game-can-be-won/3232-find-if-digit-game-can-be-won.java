1773114bef78bacbaa5493839a2534f7e899fb46
Menaha Kumari <menahakumari635@gmail.com>
2026-01-31 18:50:21 +0530

    Time: 1 ms (94.46%), Space: 45.4 MB (81.3%) - LeetHub

---
+++ 3232-find-if-digit-game-can-be-won/3232-find-if-digit-game-can-be-won.java
@@ -0,0 +1,10 @@
+class Solution {
+    public boolean canAliceWin(int[] nums) {
+        int s1=0;
+        int s2=0;
+        for(int i=0;i<nums.length;i++){
+            if(nums[i]<10) s1+=nums[i];
+            else s2+=nums[i];
+        } return s1!=s2; 
+    }
+}
